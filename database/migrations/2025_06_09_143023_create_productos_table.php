cb1a51a42dfe59cce7e3065f95da1acbd95a26d9
Jose Sissa <sissamendoza@outlook.com>
2025-07-09 11:00:40 -0500

    Soft deletes

--- database/migrations/2025_06_09_143023_create_productos_table.php
+++ database/migrations/2025_06_09_143023_create_productos_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->decimal('precio', 8, 2);
             $table->string('descripcion');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
