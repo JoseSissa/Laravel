577b1ed59b7ae96233c23b8e75b345019d6f2457
Jose Sissa <sissamendoza@outlook.com>
2025-07-09 10:07:07 -0500

    Function POST with custom messages

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
@@ -16,5 +18,29 @@ class ProductController extends Controller
         $products = Product::skip($offset)->take($perPage)->get();
 
         return response()->json($products);
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $validatedData = $request->validate([
+                'name' => 'required|string|max:255',
+                'description' => 'required|string|max:2000',
+                'price' => 'required|numeric',
+                'category_id' => 'required|numeric|exists:category,id',
+            ], [
+                'name.required' => 'El nombre del producto es obligatorio',
+                'name.string' => 'El nombre del producto debe ser de tipo string',
+                'description.required' => 'La descripción del producto es obligatoria',
+                'price.required' => 'El precio del producto es obligatorio',
+                'category_id.required' => 'La categoría del producto es obligatoria',
+                'category_id.exists' => 'El producto no existe en la categoría',
+            ]);
+            $product = Product::create($validatedData);
+
+            return response()->json($product);
+        } catch (ValidationException $err) {
+            return response()->json($err->errors(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
     }
 }
