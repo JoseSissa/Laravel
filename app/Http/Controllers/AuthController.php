6e746f4a10bf8e435bc5da6ed35d7af6008b7e3e
Jose Sissa <sissamendoza@outlook.com>
2025-07-22 11:44:24 -0500

    Creating JWT in login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
@@ -23,5 +26,32 @@ class AuthController extends Controller
             'message' => 'Registro exitoso',
             'user' => $user,
         ], Response::HTTP_CREATED);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $validatedData = $request->validated();
+
+        $credentials = [
+            'email' => $validatedData['email'],
+            'password' => $validatedData['password'],
+        ];
+
+        try {
+            $token = JWTAuth::attempt($credentials);
+
+            if (!$token) {
+                return response()->json([
+                    'error' => 'Usuario o contraseña incorrectos',
+                ], Response::HTTP_UNAUTHORIZED);
+            }
+        } catch (JWTException $e) {
+            return response()->json([
+                'message' => 'No se pudo generar el token',
+                'error' => $e->getMessage(),
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return response()->json(['token' => $token]);
     }
 }
